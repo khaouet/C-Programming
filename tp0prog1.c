862200cc7c3dc2d8d4ee66ed768f61559a708b32
Khaled HAOUET <39202108+khaouet@users.noreply.github.com>
2024-10-21 14:37:02 +0100

    First Example
    First program using C language

---
+++ tp0prog1.c
@@ -0,0 +1,33 @@
+/*---------------------------------------------------------------------------
+  Source file : tp0prog1.c
+  Author      : Khaled HAOUET
+  Class       : L1-GE4
+  Brief       : First program using C language
+-----------------------------------------------------------------------------*/
+
+/* Inclusion of standard libraries */
+/* stdio.h is a header file        */
+/* stdlib.h is a header file       */
+
+#include <stdio.h>   // standard library for input/output
+#include <stdlib.h>  // standard library
+
+/* --------------------------------------------------------------------------
+   every C program must provide a main function which is an entry point for
+   our program. only one main function must be defined
+-----------------------------------------------------------------------------*/
+
+
+int main()
+{
+    /* printf is standard output function declared in the file <stdio.h>     */
+    /* printf is used to print a message on the screen                       */
+    /* you should use printf not print to be in conformance with the syntax  */
+    printf("Hello L1GEx-Gy!");
+    return 0;
+}
+
+
+
+
+
